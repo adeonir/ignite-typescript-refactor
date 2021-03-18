b72fb7c30e0884befda332941ab81c2078aae802
Adeonir Kohl <adeonir@gmail.com>
2021-03-18 08:11:37 -0300

    Refactor header component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 
-import Header from '../../components/Header';
 import api from '../../services/api';
+
+import { Header } from '../../components/Header';
 import Food from '../../components/Food';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
